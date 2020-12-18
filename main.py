ececfa873a5cecb298991fbc241288a6440ad5c8
lylel2 <74283145+lylel2@users.noreply.github.com>
2020-12-18 15:56:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,47 @@ def main():
 
 
 def jouer_coup(a, q, partie):
-  pass
+ """
+ Main Frame
+"""
+    #Mode automatic:
+    if a.mode_auto:
+        #on retourne un coup jouer de api.py
+        return api.jouer_coup(id, q.type_coup.upper(), q.position)
+
+    #initalise some variables
+    cap = None
+    title = "C'est à votre tour!"
+    question = "Entrez votre prochain coup (D|MH|MV) (x, y):"
+
+    #loop to determine how graphic mode functions
+    while not cap:
+        #as long as mode graphic is selected, we ask both questions
+        if a.mode_graphique:
+            #if nothing happens:
+            if entry is None:
+                #we pause the game
+                turtle.mainloop()
+                #raise an error to indicated a problem concerning window
+                raise RuntimeError('Le joueur a fermer la fenêtre')
+            #if something is entered
+            else:
+                print(question, end='')
+                #the entry is manually entered
+                entry = input()
+
+            #we use the re module to separate the values
+            #we use ^ to match the pattern at the start of the string
+            #we use $ to match the end of the string
+            #we use \s to match a single whitespace character like
+            #we use \d to match decimal digit 0-9
+            #we add them because (position, x, y)
+            #we use IGNORECASE to match lower case values
+            cap = re.search(r'(D|MH|MV)\s + (\d)\s + (\d)$', entry.split(), re.IGNORECASE)
+
+            #if we enter something
+            if cap:
+                entry = cap.groups()
 
 
 if __name__ == "__main__":
