31248dcff28e77f9e866cec4ff8e689a6ba2e700
lylel2 <74283145+lylel2@users.noreply.github.com>
2020-12-18 14:31:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,22 @@ from quoridor import Quoridor
 from quoridorx import QuoridorX
 
 def analyser_commande():
-
-    parser = argparse.ArgumentParser(description = 'Jeu Quoridor')
-    parser.add_argument('-p', 'lister', action='store_true', help='Lister les identifiants des 20 dernières parties')
-    parser.add_agument('idul', help = 'IDUL du joueur')
-    parser.add_argument("-a", dest="mode_auto", action="store_true", help="Jouer contre le serveur en mode automatique.")
-    parser.add_argument('-x', dest="mode_graphique", action="store_true", help="Jouer contre le serveur en mode manuel avec affichage graphique.")
-    parser.add_argument('-ax', dest="mode_graphique", action="store_true", help="Jouer contre le serveur en mode auto avec affichage graphique.")
+    """
+    TP1
+    """
+    # Créer un analyseur de ligne de commande
+    parser = argparse.ArgumentParser(description="Jeu Quoridor")
+    # positional argument for the player's IDUL:
+    parser.add_argument('IDUL', metavar='idul', help="IDUL du joueur")
+    #Optional argument for auto-mode
+    parser.add_argument('-a', dest="mode_auto", action='store_true', help='Activer le mode automatique')
+    #Optional argument to activate graphic
+    parser.add_argument('-x', dest="mode_graphique", action='store_true', help='Activer le mode graphique')
+    # optional arguments:
+    parser.add_argument(
+        '-p', '--parties', action='store_true', 
+        help='Lister les identifiants de vos 20 dernières parties'
+    )
     return parser.parse_args()
 
 
